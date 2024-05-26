1c293c8dc40d3a5182c5c458efcb4127b3a7ad3a
NykielJ <jnykiel437r60@gmail.com>
2024-05-26 14:34:05 +0200

    Create GameBoard creator

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Memory app',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        fontFamily: 'Arial',
       ),
       home: const MyHomePage(title: 'Memory App'),
     );
@@ -37,15 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -53,40 +29,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  void _navigateToGameBoard(BuildContext context, int gridSize) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => GameBoard(gridSize: gridSize)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return  Scaffold(
+    return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Text(
-            widget.title,
-        style: const TextStyle(
+          widget.title,
+          style: const TextStyle(
             fontSize: 47,
             fontWeight: FontWeight.bold,
+            shadows: [
+              Shadow(
+                offset: Offset(2.0, 2.0),
+                blurRadius: 3.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ],
           ),
         ),
         centerTitle: true,
-        elevation: 0, // shadow off
+        elevation: 0,
         backgroundColor: Colors.transparent,
         leading: IconButton(
           icon: const Icon(
-                Icons.settings,
-                size: 32,
-            ),
-          onPressed: (){
-          // TODO options
+            Icons.settings,
+            size: 32,
+          ),
+          onPressed: () {
+            // TODO options
           },
         ),
-    ),
-
+      ),
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -98,86 +79,101 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ),
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'Wybierz poziom trudności',
-              style: TextStyle(fontSize:  30,
-                // shadows: [
-                //   Shadow(
-                //     color: Colors.black,
-                //     offset: Offset(0.5,0.5),
-                //   ),
-                // ], Shadows if needed
-              ),
+              style: TextStyle(fontSize: 30),
             ),
             const SizedBox(height: 30),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 ElevatedButton(
-                  onPressed: (){
-                    // TODO if pressed switch to difficulty lvl
-                    },
+                  onPressed: () {
+                    _navigateToGameBoard(context, 4);
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color(0xffb298ff),
                     shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    minimumSize: const Size(120,120),
+                        borderRadius: BorderRadius.circular(10)),
+                    minimumSize: const Size(120, 120),
                   ),
                   child: const Padding(
                     padding: EdgeInsets.all(16.0),
-                    child: Text('4x4',
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.black
+                    child: Text(
+                      '4x4',
+                      style: TextStyle(fontSize: 24, color: Colors.black),
                     ),
-                    ),
-                    ),
+                  ),
                 ),
                 const SizedBox(width: 40),
-                ElevatedButton(onPressed: ()
-                    {
-                      // TODO if pressed switch to difficulty lvl
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xffb298ff),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)
-                      ),
-                      minimumSize: const Size(120,120),
+                ElevatedButton(
+                  onPressed: () {
+                    _navigateToGameBoard(context, 8);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffb298ff),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    minimumSize: const Size(120, 120),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      '8x8',
+                      style: TextStyle(fontSize: 24, color: Colors.black),
                     ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Text('8x8',
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.black
-                      ),
-                    ),
-                    ),
+                  ),
                 ),
               ],
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class GameBoard extends StatelessWidget {
+  final int gridSize;
+
+  const GameBoard({super.key, required this.gridSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$gridSize x $gridSize Game Board'),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double cardSize = (constraints.maxWidth - (gridSize - 1) * 10) / gridSize;
+
+          return GridView.builder(
+            padding: const EdgeInsets.all(16.0),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: gridSize,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+            ),
+            itemCount: gridSize * gridSize,
+            itemBuilder: (context, index) {
+              return Container(
+                width: cardSize,
+                height: cardSize,
+                color: Colors.blueAccent,
+                child: Center(
+                  child: Text(
+                    '${index + 1}',
+                    style: const TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
